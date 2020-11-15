c1c64df15952a566c13f371dd1288a20894f5d93
ashwini-8 <ashwinip309@gmail.com>
2020-11-15 19:38:21 +0530

    Added:use case 1 added

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,1 +1,13 @@
-echo "welcome to Employee wage computation"
+#echo "welcome to Employee wage computation"
+
+
+#! /bin/bash
+isPresent=1
+random=$(( RANDOM%2 ))
+echo "Random number: "$random
+if(( $random == $isPresent ))
+then
+echo "Employee is Present!"
+else
+echo "Employee is absent!"
+fi
