dbda4e7dfba9c6f388c100782c81ce59c41ae99a
ashwini-8 <ashwinip309@gmail.com>
2020-11-15 19:42:52 +0530

    Added:use case 2 added

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -11,3 +11,21 @@ echo "Employee is Present!"
 else
 echo "Employee is absent!"
 fi
+
+
+#! /bin/bash
+isPresent=1
+random=$(( RANDOM%2 ))
+echo "Random number: "$random
+if(( $random == $isPresent ))
+then
+echo "Employee is Present!"
+empHrs=8
+ratePerHour=20
+salary=$(( $empHrs * $ratePerHour))
+else
+echo "Employee is absent!"
+salary=0
+fi
+
+echo "Salary: $salary"
